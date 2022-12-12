5f5bb823e726f48c4d62be456cb4781241a39a6a
ayushhpatel <84513508+ayushhpatel@users.noreply.github.com>
2022-12-12 16:45:47 +0530

    Add files via upload

--- server/db.js
+++ server/db.js
@@ -1,1 +1,17 @@
+const mongoose=require('mongoose');
 
+var mongoURL='mongodb+srv://ayushhh:1Mgog0FEcCj4Ad0z@cluster0.tpuz8os.mongodb.net/mern-hotel';
+
+mongoose.connect(mongoURL,{useUnifiedTopology:true, useNewUrlParser:true})
+
+var connection=mongoose.connection;
+
+connection.on('error',()=>{
+    // console.log("Connection failed");
+})
+
+connection.on('connected',()=>{
+    // console.log("Connection successful");
+})
+
+module.exports=mongoose
